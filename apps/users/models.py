bde014df1ec093b11795e57f28d8cef11e5c77c6
poetries <jingguanliuye@gmail.com>
2017-03-17 23:23:12 +0800

    update

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,4 +1,4 @@
-# _*_ encoding:utf-8 _*_
+# _*_ coding:utf-8 _*_
 
 from __future__ import unicode_literals
 from datetime import datetime
@@ -13,7 +13,7 @@ from django.contrib.auth.models import AbstractUser
 class UserProfile(AbstractUser):
     nick_name = models.CharField(max_length=50, verbose_name=u"昵称", default=u"")
     birday = models.DateField(verbose_name=u"生日", null=True, blank=True)
-    gender = models.CharField(max_length=5, choices=(("male", u"男"), ("female", u"女")), default="female")
+    gender = models.CharField(max_length=50, choices=(("male", u"男"), ("female", u"女")), default="female")
     address = models.CharField(max_length=100, default=u"")
     mobile = models.CharField(max_length=11, null=True, blank=True)
     image = models.ImageField(upload_to="image/%Y/%m", default=u"image/default.png", max_length=100)
--- onlineShop/settings.py
+++ onlineShop/settings.py
@@ -42,6 +42,9 @@ INSTALLED_APPS = [
     'courses',
     'operation',
     'organization'
+    # 'xadmin',
+    # 'crispy_forms'
+
 ]
 
 AUTH_USER_MODEL = "users.UserProfile"
@@ -115,15 +118,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
