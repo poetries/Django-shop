e78872b0f864ee15c980139d3d3bfea904c16b86
poetries <jingguanliuye@gmail.com>
2017-04-30 00:25:12 +0800

    finish login

--- apps/users/views.py
+++ apps/users/views.py
@@ -3,9 +3,12 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.backends import ModelBackend
 from django.db.models import Q
+from django.views.generic.base import View
+from django.http import HttpResponse, HttpResponsePermanentRedirect
+from django.core.urlresolvers import reverse
 
 from .models import UserProfile
-
+from .forms import LoginForm
 
 class CustomBackend(ModelBackend):
     def authenticate(self, username=None, password=None, **kwargs):
@@ -17,17 +20,45 @@ class CustomBackend(ModelBackend):
             return None
 
 
-# Create your views here.
+# 用户登录
+class LoginView(View):
+    def get(self, request):
+        return render(request, 'login.html')
 
-def user_login(request):
-    if request.method == 'POST':
-        user_name = request.POST.get("username", "")
-        pass_word = request.POST.get("password", "")
-        user = authenticate(username=user_name, password=pass_word)
-        if user is not None:
-            login(request, user)
-            return render(request, "index.html")
+    def post(self, request):
+        login_form = LoginForm(request.POST)
+        if login_form.is_valid():
+            user_name = request.POST.get('username', '')
+            password = request.POST.get('password', '')
+            # 上面的 authenticate 方法 return user
+            user = authenticate(username=user_name, password=password)
+
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponsePermanentRedirect(reverse('index'))
+                return render(request, 'login.html', {'msg': '用户未激活！'})
+            return render(request, 'login.html', {'msg': '用户名或者密码错误！'})
         else:
-            return render(request, "login.html", {"msg": "用戶名或密码错误!"})
-    elif request.method == "GET":
-        return render(request, "login.html", {})
+            return render(request, 'login.html', {'form_errors': login_form.errors})
+
+
+
+
+
+
+
+# Create your views here.  不用这种方式
+
+# def user_login(request):
+#     if request.method == 'POST':
+#         user_name = request.POST.get("username", "")
+#         pass_word = request.POST.get("password", "")
+#         user = authenticate(username=user_name, password=pass_word)
+#         if user is not None:
+#             login(request, user)
+#             return render(request, "index.html")
+#         else:
+#             return render(request, "login.html", {"msg": "用戶名或密码错误!"})
+#     elif request.method == "GET":
+#         return render(request, "login.html", {})
