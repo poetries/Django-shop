516c296ee62ceb1e371d8c7df6eda1df63e342dc
poetries <jingguanliuye@gmail.com>
2017-05-07 21:42:29 +0800

    update

--- apps/courses/views.py
+++ apps/courses/views.py
@@ -6,6 +6,7 @@ from django.http import HttpResponse
 
 from .models import Course, CourseResource
 from operation.models import UserFavorite, CourseComments, UserCourse
+from utils.mixin_utils import LoginRequiredMixin
 
 # Create your views here.
 
@@ -76,15 +77,22 @@ class CourseDetailView(View):
 
 
 # 课程章节信息
-class CourseInfoView(View):
+class CourseInfoView(LoginRequiredMixin, View):
     def get(self, request, course_id):
         course = Course.objects.get(id=int(course_id))
+
+        # 查询用户是否已经关联该课程
+        user_courses = UserCourse.objects.filter(user=request.user, course=course)
+        if not user_courses:
+            user_course = UserCourse(user=request.user, course=course)
+            user_course.save()
+
         user_courses = UserCourse.objects.filter(course=course)
         user_ids = [user_course.user.id for user_course in user_courses]
         all_user_courses = UserCourse.objects.filter(user_id__in=user_ids)
 
         #取出所有课程ID
-        course_ids = [user_course.course.id for user_course in user_courses]
+        course_ids = [user_course.course.id for user_course in all_user_courses]
 
         #获取学过该用户学过的其他所有课程
         relate_courses = Course.objects.filter(id__in=course_ids).order_by("-click_nums")[:5]
@@ -99,7 +107,7 @@ class CourseInfoView(View):
 
 
 # 课程评论
-class CommentsView(View):
+class CommentsView(LoginRequiredMixin, View):
     def get(self, request, course_id):
         course = Course.objects.get(id=int(course_id))
         all_resources = CourseResource.objects.filter(course=course)
