ce4675306136a22e7a96d08a743c96a2fd94d66c
poetries <jingguanliuye@gmail.com>
2017-05-07 10:26:49 +0800

    update

--- apps/organization/urls.py
+++ apps/organization/urls.py
@@ -1,7 +1,7 @@
 # _*_ coding:utf-8 _*_
 from django.conf.urls import url, include
 
-from .views import OrgView, AddUserAskView, OrgHomeView, OrgCourseView
+from .views import OrgView, AddUserAskView, OrgHomeView, OrgCourseView, OrgDescView, OrgTeacherView, AddFavView
 
 urlpatterns = [
     #课程机构列表页
@@ -9,4 +9,9 @@ urlpatterns = [
     url(r'^add_ask/$', AddUserAskView.as_view(), name='add_ask'),
     url(r'^home/(?P<org_id>\d+)/$', OrgHomeView.as_view(), name='org_home'),
     url(r'^course/(?P<org_id>\d+)/$', OrgCourseView.as_view(), name='org_course'),
+    url(r'^desc/(?P<org_id>\d+)/$', OrgDescView.as_view(), name='org_desc'),
+    url(r'^teacher/(?P<org_id>\d+)/$', OrgTeacherView.as_view(), name='org_teacher'),
+
+    # 机构收藏
+    url(r'^add_fav/$', AddFavView.as_view(), name='add_fav'),
 ]
--- apps/organization/views.py
+++ apps/organization/views.py
@@ -7,6 +7,7 @@ from django.http import HttpResponse
 from .models import CourseOrg, CityDict
 from .forms import UserAskForm
 from courses.models import Course
+from operation.models import UserFavorite
 
 # Create your views here.
 
@@ -101,3 +102,55 @@ class OrgCourseView(View):
             'course_org': course_org,
             'current_page': current_page
         })
+
+
+# 机构介绍页
+class OrgDescView(View):
+    def get(self, request, org_id):
+        current_page = "desc"
+        course_org = CourseOrg.objects.get(id=int(org_id))
+        return render(request, 'org-detail-desc.html', {
+            'course_org': course_org,
+            'current_page': current_page
+        })
+
+
+# 机构教师页
+class OrgTeacherView(View):
+    def get(self, request, org_id):
+        current_page = "teacher"
+        course_org = CourseOrg.objects.get(id=int(org_id))
+        all_teachers = course_org.teacher_set.all()
+        return render(request, 'org-detail-teachers.html', {
+            'all_teachers': all_teachers,
+            'course_org': course_org,
+            'current_page': current_page
+        })
+
+
+# 用户收藏 用户取消收藏
+class AddFavView(View):
+    def post(self, request):
+        fav_id = request.POST.get('fav_id', 0)
+        fav_type = request.POST.get('fav_type', 0)
+
+        if not request.user.is_authenticated():
+            # 判断用户登录状态
+            return HttpResponse('{"status": "fail", "msg": "用户未登录"}', content_type='application/json')
+        exist_records = UserFavorite.objects.filter(user=request.user, fav_id=int(fav_id), fav_type=int(fav_type))
+        if exist_records:
+            # 如果记录已经存在 表示用户曲线收藏
+            exist_records.delete()
+            return HttpResponse('{"status": "success", "msg": "收藏"}', content_type='application/json')
+        else:
+            user_fav = UserFavorite()
+            if int(fav_id) > 0 and int(fav_type) > 0:
+               user_fav.user = request.user
+               user_fav.fav_id = int(fav_id)
+               user_fav.fav_type = int(fav_type)
+               user_fav.save()
+               return HttpResponse('{"status": "success", "msg": "已收藏"}', content_type='application/json')
+            else:
+                return HttpResponse('{"status": "fail", "msg": "收藏出错"}', content_type='application/json')
+
+
