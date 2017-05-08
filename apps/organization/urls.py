6c3a7c5bff1a3a6cb81c1213f7ab02035253b328
poetries <jingguanliuye@gmail.com>
2017-05-08 21:47:05 +0800

    update

--- apps/organization/urls.py
+++ apps/organization/urls.py
@@ -1,7 +1,7 @@
 # _*_ coding:utf-8 _*_
 from django.conf.urls import url, include
 
-from .views import OrgView, AddUserAskView, OrgHomeView, OrgCourseView, OrgDescView, OrgTeacherView, AddFavView, TeacherListView
+from .views import OrgView, AddUserAskView, OrgHomeView, OrgCourseView, OrgDescView, OrgTeacherView, AddFavView, TeacherListView, TeacherDetailView
 
 
 urlpatterns = [
@@ -18,4 +18,7 @@ urlpatterns = [
 
     # 讲师列表页
     url(r'^teacher/list$', TeacherListView.as_view(), name='teacher_list'),
+
+    # 讲师详情页
+    url(r'^teacher/detail/(?P<teacher_id>\d+)/$', TeacherDetailView.as_view(), name='teacher_detail'),
 ]
--- apps/organization/views.py
+++ apps/organization/views.py
@@ -203,3 +203,30 @@ class TeacherListView(View):
             'sort': sort
         })
 
+
+# 讲师详情
+class TeacherDetailView(View):
+    def get(self, request, teacher_id):
+        sorted_teacher = Teacher.objects.all().order_by('-click_nums')[:3]
+
+        teacher = Teacher.objects.get(id=int(teacher_id))
+        teacher.click_nums += 1
+        teacher.save()
+        all_courses = Course.objects.filter(teacher=teacher)
+
+        has_teacher_faved = False
+        # 注意这里有个坑就是 teacher_id 是字符串，teacher.id 是数字
+        if UserFavorite.objects.filter(user=request.user, fav_type=3, fav_id=teacher.id):
+            has_teacher_faved = True
+
+        has_org_faved = False
+        if UserFavorite.objects.filter(user=request.user, fav_type=2, fav_id=teacher.org.id):
+            has_org_faved = True
+
+        return render(request, 'teacher-detail.html', {
+            'teacher': teacher,
+            'all_courses': all_courses,
+            'sorted_teacher': sorted_teacher,
+            'has_teacher_faved': has_teacher_faved,
+            'has_org_faved': has_org_faved,
+        })
