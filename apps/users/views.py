15d66a6492920590551acfbd61cd220cbf01b7e9
poetries <jingguanliuye@gmail.com>
2017-05-11 22:17:40 +0800

    update

--- apps/users/views.py
+++ apps/users/views.py
@@ -338,7 +338,7 @@ class IndexView(View):
     def get(self, request):
         # 取出轮播图
         all_banners = Banner.objects.all().order_by('index')
-        courses = Course.objects.filter(is_banner=False)[:5]
+        courses = Course.objects.filter(is_banner=False)[:6]
         banner_courses = Course.objects.filter(is_banner=True)[:3]
         course_orgs = CourseOrg.objects.all()[:15]
         return render(request, 'index.html', {
